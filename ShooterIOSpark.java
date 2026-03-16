bab7bd6e9ab9ec9ef72c9e1f9d5401498b4cf4da
emilroy75 <124095483+emilroy75@users.noreply.github.com>
2026-03-16 16:22:39 -0400

    Update ShooterIOSpark.java

--- ShooterIOSpark.java
+++ ShooterIOSpark.java
@@ -1,11 +1,11 @@
-package frc.root.subsystems.shooter;
+package frc.robot.subsystems.shooter;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.RelativeEnccoder;
+import com.revrobotics.RelativeEncoder;
 
-public class ShooterIOSpark implements ShooterIO{
+public class ShooterIOSpark implements ShooterIO {
 
     private final CANSparkMax motor1;
     private final CANSparkMax motor2;
@@ -14,42 +14,42 @@ public class ShooterIOSpark implements ShooterIO{
     private final RelativeEncoder encoder2;
     private final RelativeEncoder encoder3;
 
-    public ShooterIOSpark(int canId1, int canId2, int canId3){
+    public ShooterIOSpark(int canId1, int canId2, int canId3) {
         motor1 = new CANSparkMax(canId1, MotorType.kBrushless);
-        motor2 = new CANSparkMax(canId2, MotorType.Kbrushless);
-        motor3 = new CANSparkMax(canId3, Motor type.kBrushless);
+        motor2 = new CANSparkMax(canId2, MotorType.kBrushless);
+        motor3 = new CANSparkMax(canId3, MotorType.kBrushless);
 
         encoder1 = motor1.getEncoder();
         encoder2 = motor2.getEncoder();
         encoder3 = motor3.getEncoder();
 
-        for(CANSparkMax motor : new CANSparkMax[]{motor1, motor2, motor3}){
-            motor.restoreFactoryDefualts();
+        for (CANSparkMax motor : new CANSparkMax[]{motor1, motor2, motor3}) {
+            motor.restoreFactoryDefaults();
             motor.setSmartCurrentLimit(40);
             motor.setIdleMode(IdleMode.kCoast);
             motor.burnFlash();
         }
-   }
+    }
 
     @Override
-        public void updateInputs(ShooterIOInputs inputs){
-            inputs.velocityRPM = encoder1.getVelocity();
-            inputs.appliedVolts = motor1.getAppliedOutput() * motor1.getBusVoltage();
-            inputs.currentAmps = getOutputCurrent();
-            inputs.motorConnected = true;
-        }
-
-    @override
-        public void setSpeed(double speed){
-            motor1.set(speed)
-            motor2.set(speed)
-            motor3 set(speed)
-        }
-
-    @override
-        public void stop(){
-            motor1.set(0.0)
-            motor2.set(0.0)
-            motor3.set(0.0)
-        }
+    public void updateInputs(ShooterIOInputs inputs) {
+        inputs.velocityRPM = encoder1.getVelocity();
+        inputs.appliedVolts = motor1.getAppliedOutput() * motor1.getBusVoltage();
+        inputs.currentAmps = motor1.getOutputCurrent();
+        inputs.motorConnected = true;
     }
+
+    @Override
+    public void setSpeed(double speed) {
+        motor1.set(speed);
+        motor2.set(speed);
+        motor3.set(speed);
+    }
+
+    @Override
+    public void stop() {
+        motor1.set(0.0);
+        motor2.set(0.0);
+        motor3.set(0.0);
+    }
+}
